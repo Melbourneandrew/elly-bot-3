ebb994819b230825ad3d91fe92700d2d6f416eaa
Melbourneandrew <melbourneandrew@gmail.com>
2024-01-18 01:45:49 -0600

    add loggin in JWT verification

--- api/auth/verifyJWT.ts
+++ api/auth/verifyJWT.ts
@@ -10,16 +10,19 @@ export default function verifyJWT(
   res: Response,
   next: any
 ) {
-  console.log(req.cookies);
+  console.log("Verifying JWT");
   const token = req.cookies.token;
   if (!token) {
+    console.log("No token provided");
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
     const decoded = jwt.verify(token, TOKEN_HASH);
     req.user = decoded;
+    console.log("JWT verified");
     next();
   } catch (error) {
+    console.log("Invalid token");
     return res.status(403).json({ message: "Invalid token" });
   }
 }
